08990251998c22caff85fc4c089f9abdc3ec222f
pollakit <60090031@kmitl.ac.th>
2021-06-02 10:47:35 +0700

    Debug = True

--- nearme/settings.py
+++ nearme/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 # DEBUG = config('DEBUG', default=False, cast=bool)
 
 
