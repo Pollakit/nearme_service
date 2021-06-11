15f70fa7752a2fa850b00f985184886ed57744ed
Passawit-Umrod <44994800+Passawit-Umrod@users.noreply.github.com>
2021-06-11 12:24:12 +0700

    Clearing main branch

--- accounts/admin.py
+++
@@ -1,8 +0,0 @@
-# accounts\admin.py
-from django.contrib import admin
-from .models import CustomUser, Customer, ShopOwner, MarketAdmin
-
-admin.site.register(CustomUser)
-admin.site.register(Customer)
-admin.site.register(ShopOwner)
-admin.site.register(MarketAdmin)
--- orders/admin.py
+++
@@ -1,7 +0,0 @@
-# orders\admin.py
-from django.contrib import admin
-from .models import Order, OrderedDecorator, OrderedProduct
-
-admin.site.register(Order)
-admin.site.register(OrderedDecorator)
-admin.site.register(OrderedProduct)
